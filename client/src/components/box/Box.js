86da7d965fdad230ec31947226eff97da67ddfe6
Duy Nguyen <duynguyenngoc@hotmail.com>
2021-10-21 15:28:32 +0700

    dashboard add total

--- client/src/components/box/Box.js
+++ client/src/components/box/Box.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { LikeOutlined } from '@ant-design/icons';
 
-import { Statistic, Card, Row, Col } from 'antd';
-import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 
 export default class Box extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            name: this.props.name,
+            value: this.props.value,
+            currency: this.props.currency,
+            icon: this.props.icon
+        }
        
     }
     render(){
         return(
             <div className='box'>
-                <div className='title'>
-                     <h1>{this.props.name}</h1>  
+                <div className='box-title'>
+                    <h1>{this.state.name}</h1>  
+                </div>
+                <div className='box-avatar'>
+                    <img src={this.state.icon} alt={this.state.icon}></img>
+                </div>
+                <div className='box-value'>
+                    <h1>{this.state.value}{this.state.currency}</h1>
                 </div>
             
             </div>
--- client/src/components/dashboard/DashBoardTest.js
+++ client/src/components/dashboard/DashBoardTest.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import VerticalBar from '../chart/bar/VerticalBar';
 import Polar from '../chart/other/Polar';
-import LineChart from '../chart/line/LineChart';
+// import LineChart from '../chart/line/LineChart';
 import Box from '../box/Box';
+// import {EuroCircleOutlined } from '@ant-design/icons'
+import GameIcon from '../../assets/images/dashboard/Game.png'
+import DeveloperIcon from '../../assets/images/dashboard/Developer.png'
+import UserIcon from '../../assets/images/dashboard/User.png'
+import IngameIcon from '../../assets/images/dashboard/Ingame.png'
+import AdsIcon from '../../assets/images/dashboard/Ads.png'
+import CurrencyIcon from '../../assets/images/dashboard/Currency.png'
+
+
+
+
+
+
 
 export default class DashBoardTest extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+
         }
     }
     componentDidMount(){
@@ -18,12 +32,12 @@ export default class DashBoardTest extends React.Component{
             <div className='dashboard'>
                 <div className='filter'></div>
                <div className='total'>
-                   <div className=''><Box name="Total Games"/></div>
-                   <div className='item'><Box name="Total Developer"/></div>
-                   <div className='item'><Box name="Total Users"/></div>
-                   <div className='item'><Box name="Time Playing"/></div>
-                   <div className='item'><Box name="Time Ads"/></div>
-                   <div className='item'><Box name="Total Revenues"/></div>
+                   <div className=''><Box name="Total Games" value = "154533" currency="" icon={GameIcon}/></div>
+                   <div className='item'><Box name="Total Developer" value = "2" currency="" icon={DeveloperIcon}/></div>
+                   <div className='item'><Box name="Total Users" value = "154533" currency="" icon={UserIcon}/></div>
+                   <div className='item'><Box name="Time Playing" value = "904533" currency="s" icon={IngameIcon}/></div>
+                   <div className='item'><Box name="Time Ads" value = "904533" currency="s" icon={AdsIcon}/></div>
+                   <div className='item'><Box name="Total Revenues" value = "21050" currency="$" icon={CurrencyIcon}/></div>
                </div>
               
                <div className='col-one'>
